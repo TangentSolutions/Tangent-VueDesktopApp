263c7a1e818eeaee93d91b54e6ab2531d23cfe36
toast38coza <info@38.co.za>
2017-03-03 15:09:24 +0200

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-col :span="24">
+      <app-header></app-header>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="8">
+        <!-- should be: <side-nav :items='..' ></side-nav> -->
+        <side-nav></side-nav>
+      </el-col>
+      <el-col :span="16">
+        <img src="./assets/logo.png">
+        <router-view></router-view>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/pages/AppHeader'
+import SideNav from './components/pages/SideNav'
+export default {
+  name: 'app',
+  components: { AppHeader, SideNav }
+}
+</script>
+
+<style>
+body{
+  margin:0px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
